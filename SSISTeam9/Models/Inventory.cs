2e387952a19a2a6f8daebbd3a5ff7dd2130a0b02
Soe Ya Htet <shakusoe@gmail.com>
2019-07-30 15:01:38 +0800

    Inventory change.

--- SSISTeam9/Models/Inventory.cs
+++ SSISTeam9/Models/Inventory.cs
@@ -7,74 +7,15 @@ namespace SSISTeam9.Models
 {
     public class Inventory
     {
-        private long itemId;
-        private string itemCode;
-        private string binNo;
-        private int stockLevel;
-        private int reorderLevel;
-        private int reorderQty;
-        private string category;
-        private string description;
-        private string unitOfMeasure;
-        private string imageUrl;
-
-        public long ItemId
-        {
-            get { return itemId; }
-        }
-
-        public string ItemCode
-        {
-            get { return itemCode; }
-            set { itemCode = value; }
-        }
-
-        public string BinNo
-        {
-            get { return binNo; }
-            set { binNo = value; }
-        }
-
-        public int StockLevel
-        {
-            get { return stockLevel; }
-            set { stockLevel = value; }
-        }
-
-        public int ReorderLevel
-        {
-            get { return reorderLevel; }
-            set { reorderLevel = value; }
-        }
-
-        public int ReorderQty
-        {
-            get { return reorderQty; }
-            set { reorderQty = value; }
-        }
-
-        public string Category
-        {
-            get { return category; }
-            set { category = value; }
-        }
-
-        public string Description
-        {
-            get { return description; }
-            set { description = value; }
-        }
-
-        public string UnitOfMeasure
-        {
-            get { return unitOfMeasure; }
-            set { unitOfMeasure = value; }
-        }
-
-        public string ImageUrl
-        {
-            get { return imageUrl; }
-            set { imageUrl = value; }
-        }
+        public long ItemId { get; }
+        public string ItemCode { get; set; }
+        public string BinNo { get; set; }
+        public int StockLevel { get; set; }
+        public int ReorderLevel { get; set; }
+        public int ReorderQty { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string UnitOfMeasure { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
