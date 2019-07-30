1508d8bef07fae45ffbc413e9630d8e1b5c78e61
Nway Sandi Oo <e0395885@u.nus.edu>
2019-07-30 15:43:46 +0800

    testing github

--- SSISTeam9/Models/Inventory.cs
+++ SSISTeam9/Models/Inventory.cs
@@ -17,6 +17,5 @@ namespace SSISTeam9.Models
         public string Description { get; set; }
         public string UnitOfMeasure { get; set; }
         public string ImageUrl { get; set; }
-        
     }
 }
